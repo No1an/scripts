1806d80524d0168cce50c66282b0c5209d077a4e
Roman <r.bovda@gmail.com>
2015-10-20 18:34:12 +0300

    update grep

--- remove trojan code/remove_trojans.sh
+++ remove trojan code/remove_trojans.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir list
-grel -l '<?php if(!isset($GLOBALS.' /var/www/html/* > list
+grel -p '<?php if(!isset($GLOBALS.' /var/www/html/* > list
 while read p;
    do sed -i -e 's/<?php if(!isset($GLOBALS.*/<?php/g' $p;
    #echo "cleaned file"; 
