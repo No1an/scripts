4df36020b133b0f79459a2b8152c6cf8b33fe118
Roman <r.bovda@gmail.com>
2015-10-20 17:51:23 +0300

    Add grep and write to list

--- remove trojan code/remove_trojans.sh
+++ remove trojan code/remove_trojans.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir list
+grel -l '<?php if(!isset($GLOBALS.' /var/www/html/* > list
 while read p;
    do sed -i -e 's/<?php if(!isset($GLOBALS.*/<?php/g' $p;
    #echo "cleaned file"; 
